f10d5e722a0106d5a53f859f8610b0f1a0641df7
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2023-07-10 17:46:11 +0600

    client:dummy-profile-card-styling

---
+++ client/src/components/ProfileCard.jsx
@@ -0,0 +1,161 @@
+import { Link } from "react-router-dom";
+
+const ProfileCard = () => {
+  return (
+    <div className="max-w-3xl md:m-2 w-full border-2 m-3 px-8 py-4">
+      <div className="mb-6">
+        <img
+          className="w-24 h-24 mb-3 rounded-full shadow-lg"
+          src="/docs/images/people/profile-picture-3.jpg"
+          alt="Bonnie image"
+        />
+        <h5 className="mb-1 text-2xl font-bold text-gray-900 dark:text-white">
+          Bonnie Green
+        </h5>
+        <span className="text-sm text-gray-500 dark:text-gray-400">
+          Visual Designer
+        </span>
+      </div>
+      <div className="flex flex-col mb-6 justify-between">
+        <h3 className=" text-xl font-medium text-gray-900 dark:text-white">
+          General Information
+        </h3>
+        <p className="my-2 text-lg font-normal text-gray-900 dark:text-white">
+          About Me
+        </p>
+        <p className=" text-md font-normal text-gray-900 dark:text-white">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum cum
+          nesciunt libero beatae explicabo ea at nemo, quia eligendi quidem, quo
+          molestiae animi quod enim itaque consequuntur reiciendis tempore
+          dicta!
+        </p>
+      </div>
+      <div className="mb-6">
+        <p className="mb-3 text-lg font-normal text-gray-900 dark:text-white">
+          Skills
+        </p>
+        <div>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            HTML
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            CSS
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            JavaScript
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            HTML
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            CSS
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            JavaScript
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            HTML
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            CSS
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            JavaScript
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            HTML
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            CSS
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            JavaScript
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            HTML
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            CSS
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            JavaScript
+          </span>
+        </div>
+      </div>
+      <div className="mb-6">
+        <p className="mb-3 text-lg font-normal text-gray-900 dark:text-white">
+          Hobbies
+        </p>
+        <div className="">
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            HTML
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            CSS
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            JavaScript
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            HTML
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            CSS
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            JavaScript
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            HTML
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            CSS
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            JavaScript
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            HTML
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            CSS
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            JavaScript
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            HTML
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            CSS
+          </span>
+          <span className="inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            JavaScript
+          </span>
+        </div>
+      </div>
+      <div>
+        <Link
+          to="profile"
+          className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-sky-800 hover:bg-sky-600 active:bg-sky-700 duration-150 rounded-full md:inline-flex"
+        >
+          Edit
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileCard;
--- client/src/pages/Main/Profile.jsx
+++ client/src/pages/Main/Profile.jsx
@@ -1,3 +1,4 @@
+import ProfileCard from "../../components/ProfileCard";
 import { useUserDetailsQuery } from "../../features/auth/authApi";
 import useAuth from "../../hooks/useAuth";
 
@@ -7,11 +8,9 @@ const Profile = () => {
     const {data,isLoading}=useUserDetailsQuery(email);
     console.log(data)
     return (
-        <section className="w-full flex items-center justify-center">
-            <div className="max-w-sm w-full bg-slate-400 m-3">
-                Username: {data && data?.username}
-            </div>
-            <div className="max-w-sm bg-slate-400 m-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, mollitia odit omnis ad similique provident architecto odio et, exercitationem doloribus neque voluptatibus maxime earum praesentium sunt esse corporis dolores nemo.</div>
+        <section className="w-full md:flex-row flex flex-col items-center justify-center">
+            <ProfileCard/>
+
         </section>
     );
 };
